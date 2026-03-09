09f3a6dcec8deb6af0460b23ed9d3049ffa9287b
ankushoffice-commits <ankushoffice10@gmail.com>
2026-03-09 10:10:04 +0530

    Create SimpleTwoSum.java

---
+++ SimpleTwoSum.java
@@ -0,0 +1,27 @@
+class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target){
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indexes: " + result[0] + " , " + result[1]);
+    }
+}
